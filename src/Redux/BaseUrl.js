15c64302587b1d828fe9da5d939b329759e8aa19
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2024-02-09 13:10:06 +0530

    Final Issues

--- src/Redux/BaseUrl.js
+++ src/Redux/BaseUrl.js
@@ -1,7 +1,5 @@
-const BaseUrl = "https://walrus-app-g9c8k.ondigitalocean.app/api";
-// const BaseUrl = "http://localhost:5000/api";
-// const AssetsUrl = "http://localhost:5000";
-const AssetsUrl = "https://walrus-app-g9c8k.ondigitalocean.app";
+const BaseUrl = "https://coimbatoreplaces-e9d3ecd6083c.herokuapp.com/api";
+const AssetsUrl = "https://coimbatoreplaces-e9d3ecd6083c.herokuapp.com";
 const APIKey =
   "pk.eyJ1IjoiYW5zYXJpYnJhaGltIiwiYSI6ImNscW0ybmc0NjAxc2YyaXNicGtzM25saXcifQ.G9cUWgFtRkJlVN7XID3_ZQ";
 
